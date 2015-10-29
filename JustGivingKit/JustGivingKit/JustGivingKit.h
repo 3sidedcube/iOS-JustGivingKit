f006147348bb45d10fa11a4c0ec35b16a3ef0a02
Sam Houghton <sam@3sidedcube.com>
2015-10-29 09:06:37 +0000

    Initial commit

---
+++ JustGivingKit/JustGivingKit/JustGivingKit.h
@@ -0,0 +1,19 @@
+//
+//  JustGivingKit.h
+//  JustGivingKit
+//
+//  Created by Sam Houghton on 29/10/2015.
+//  Copyright © 2015 3 Sided Cube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JustGivingKit.
+FOUNDATION_EXPORT double JustGivingKitVersionNumber;
+
+//! Project version string for JustGivingKit.
+FOUNDATION_EXPORT const unsigned char JustGivingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JustGivingKit/PublicHeader.h>
+
+
