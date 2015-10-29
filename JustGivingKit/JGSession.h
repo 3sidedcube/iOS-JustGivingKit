fb927ec8491afca514c3d7ba04e7629e906af082
Sam Houghton <sam@3sidedcube.com>
2015-10-29 09:49:13 +0000

    Adds session object and framework references

---
+++ JustGivingKit/JGSession.h
@@ -0,0 +1,13 @@
+//
+//  JGSession.h
+//  JustGivingKit
+//
+//  Created by Sam Houghton on 29/10/2015.
+//  Copyright © 2015 3 Sided Cube. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JGSession : NSObject
+
+@end
---
+++ JustGivingKit/JGSession.m
@@ -0,0 +1,15 @@
+//
+//  JGSession.m
+//  JustGivingKit
+//
+//  Created by Sam Houghton on 29/10/2015.
+//  Copyright © 2015 3 Sided Cube. All rights reserved.
+//
+
+#import "JGSession.h"
+@import ThunderBasics;
+@import ThunderRequest;
+
+@implementation JGSession
+
+@end
