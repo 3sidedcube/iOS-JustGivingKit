4eb1795b81d09d62f95f5a5708ce4d3c07e0086f
Joel Trew <joel@3sidedcube.com>
2015-11-12 17:00:08 +0000

    Adds charity controller

---
+++ JustGivingKit/JGCharityController.h
@@ -0,0 +1,13 @@
+//
+//  JGCharityController.h
+//  JustGivingKit
+//
+//  Created by Joel Trew on 12/11/2015.
+//  Copyright © 2015 3 Sided Cube. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JGCharityController : NSObject
+
+@end
---
+++ JustGivingKit/JGCharityController.m
@@ -0,0 +1,13 @@
+//
+//  JGCharityController.m
+//  JustGivingKit
+//
+//  Created by Joel Trew on 12/11/2015.
+//  Copyright © 2015 3 Sided Cube. All rights reserved.
+//
+
+#import "JGCharityController.h"
+
+@implementation JGCharityController
+
+@end
