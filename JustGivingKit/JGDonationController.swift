9c62ff7b5b176ce4e02e8e16ace0fab410c198de
Matthew Cheetham <matt@3sidedcube.com>
2015-12-23 13:57:26 +0000

    Adds support for GetFundraisingPageDonations. Closes #22

--- JustGivingKit/JGDonationController.swift
+++ JustGivingKit/JGDonationController.swift
@@ -14,13 +14,35 @@ Handles loading of donations in the JustGiving API
 public extension JGDonationController {
     
     /**
-    Gets an array of donations for a fundraising page
-    - parameter fundraisingPage: The fundraising page to load donations for
+    Gets an array of donations for a fundraising page. Results are paginated and will be loaded 50 at a time, you can load more by using the pageNumber parameter
+    - parameter fundraisingPage: The fundraising page to load donations for. This starts at 1.
+    - parameter pageNumber: The page number to load. Used for basic pagination.
     - parameter completion: The closure to call when the request succeeds or fails
     */
-    public func getDonationsForFundraisingPage(fundraisingPage: JGFundraisingPage?, completion: (error: NSError?) -> Void) {
+    public func getDonationsForFundraisingPage(fundraisingPage: JGFundraisingPage!, pageNumber: Int!, completion: (donations: [JGDonation]?, error: NSError?) -> Void) {
         
-        
+        JGSession.sharedSession().requestController.get("account/donations?pagenum=\(pageNumber)") { (response: TSCRequestResponse?, error: NSError?) -> Void in
+            
+            if let requestError = error {
+                
+                completion(donations: nil, error: requestError)
+                return
+            }
+            
+            if let responseDictionary = response?.dictionary as? [String: AnyObject], let donationsArray = responseDictionary["donations"] as? [[String: AnyObject]] {
+                
+                let donations = donationsArray.map({
+                    JGDonation(dictionary: $0)
+                }) as? [JGDonation]
+                
+                completion(donations: donations, error: nil)
+                return
+                
+            }
+            
+            let error = NSError(domain: "com.threesidedcube.JustGivingKit", code: 204, userInfo: [NSLocalizedDescriptionKey: "No donations found"])
+            completion(donations: nil, error: error)
+        }
         
     }
 
