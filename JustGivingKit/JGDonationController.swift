c9530fadc4236c77c0bde16b6ccbed3a0beffd77
Matthew Cheetham <matt@3sidedcube.com>
2015-12-23 11:41:32 +0000

    Begins stubbing out donations loading

---
+++ JustGivingKit/JGDonationController.swift
@@ -0,0 +1,27 @@
+//
+//  JGDonationController.swift
+//  JustGivingKit
+//
+//  Created by Matthew Cheetham on 23/12/2015.
+//  Copyright © 2015 3 Sided Cube. All rights reserved.
+//
+
+import UIKit
+
+/**
+Handles loading of donations in the JustGiving API
+*/
+public extension JGDonationController {
+    
+    /**
+    Gets an array of donations for a fundraising page
+    - parameter fundraisingPage: The fundraising page to load donations for
+    - parameter completion: The closure to call when the request succeeds or fails
+    */
+    public func getDonationsForFundraisingPage(fundraisingPage: JGFundraisingPage?, completion: (error: NSError?) -> Void) {
+        
+        
+        
+    }
+
+}
